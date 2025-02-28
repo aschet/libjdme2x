c461379b2ae367050f41deb4140229c2864d0f55
Thomas Ascher <thomas.ascher@gmx.at>
2025-02-28 20:34:05 +0100

    String lexing now grammar conform

--- src/Parser/CommandLexerDefinition.cpp
+++ src/Parser/CommandLexerDefinition.cpp
@@ -13,7 +13,7 @@ CommandLexerDefinition::CommandLexerDefinition() {
   add("[a-zA-Z][a-zA-Z0-9]*(\\.[a-zA-Z][a-zA-Z0-9]*)*", CommandTokenID::Name);
   add("[+\\-]?(\\d+(\\.\\d*)?|(\\.\\d+))([eE][+\\-]?\\d{1,3})?",
       CommandTokenID::Number);
-  add("\\\"[^\\\"]+\\\"", CommandTokenID::String);
+  add("\\\"[ -!#-~]+\\\"", CommandTokenID::String);
   // TODO: XML <[^>]+>([^<]*)<\/[^>]+>
   add("\n\r|\n|\r", CommandTokenID::Terminator);
 }
